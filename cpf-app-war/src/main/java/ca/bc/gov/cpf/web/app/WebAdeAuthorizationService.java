67d2ca75649aa11668e71544516c2610d3da5945
PXAUSTIN <PXAUSTIN@ae29540e-f523-aa4d-84ac-4afd0e2cd6fe>
2012-02-24 01:47:17 +0000

    3.0.0.DLV-SNAPSHOT
    git-svn-id: https://apps.bcgov/svn/cpf/source/trunk@144 ae29540e-f523-aa4d-84ac-4afd0e2cd6fe

--- cpf-app-war/src/main/java/ca/bc/gov/cpf/web/app/WebAdeAuthorizationService.java
+++ cpf-app-war/src/main/java/ca/bc/gov/cpf/web/app/WebAdeAuthorizationService.java
@@ -1,92 +1,41 @@
 package ca.bc.gov.cpf.web.app;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.annotation.PreDestroy;
+import javax.servlet.ServletContext;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.web.context.ServletContextAware;
 
 import ca.bc.gov.open.cpf.api.security.service.BaseAuthorizationService;
-import ca.bc.gov.webade.Action;
-import ca.bc.gov.webade.AppRoles;
 import ca.bc.gov.webade.Application;
-import ca.bc.gov.webade.Role;
-import ca.bc.gov.webade.WebADEApplicationUtils;
-import ca.bc.gov.webade.WebADEException;
+import ca.bc.gov.webade.http.HttpRequestUtils;
 import ca.bc.gov.webade.user.GUID;
 import ca.bc.gov.webade.user.UserCredentials;
 import ca.bc.gov.webade.user.WebADEUserInfo;
-import ca.bc.gov.webade.user.WebADEUserPermissions;
 import ca.bc.gov.webade.user.service.UserInfoService;
 import ca.bc.gov.webade.user.service.UserInfoServiceException;
 
-public class WebAdeAuthorizationService extends BaseAuthorizationService {
-  private static final Logger LOG = LoggerFactory.getLogger(WebAdeAuthorizationService.class);
-
-  private Map<String, Application> webAdeApplications = new HashMap<String, Application>();
-
-  @Override
-  public boolean canAccessResource(
-    final String businessApplicationName,
-    final String consumerKey,
-    final String resourceClass,
-    final String resourceId) {
-    return canAccessResource(businessApplicationName, consumerKey,
-      resourceClass, resourceId, "any");
-  }
+public class WebAdeAuthorizationService extends BaseAuthorizationService
+  implements ServletContextAware {
+  private Application application;
 
   @Override
-  public boolean canAccessResource(
-    final String businessApplicationName,
-    final String consumerKey,
-    final String resourceClass,
-    final String resourceId,
-    final String actionName) {
-    return canPerformAction(businessApplicationName, consumerKey, resourceClass
-      + ":" + resourceId + ":" + actionName);
-  }
-
-  @Override
-  public boolean canPerformAction(
-    final String businessApplicationName,
-    final String consumerKey,
-    final String actionName) {
-    final Application webAdeApplication = getWebAdeApplication(businessApplicationName);
-    if (webAdeApplication == null) {
-      return false;
-    } else {
-      final WebADEUserPermissions userPermissions = getUserPermissions(
-        businessApplicationName, consumerKey);
-      if (userPermissions == null) {
-        return false;
-      } else {
-        final boolean permission = userPermissions.canPerformAction(new Action(
-          actionName));
-        return permission;
-      }
-    }
+  public void setServletContext(final ServletContext context) {
+    this.application = HttpRequestUtils.getApplication(context);
   }
 
   @PreDestroy
   public void destroy() {
-    for (final Application application : webAdeApplications.values()) {
-      try {
-        application.shutdown();
-      } catch (final SecurityException e) {
-        LOG.error("Unable to shutdown " + application.getApplicationCode());
-      }
-    }
-    this.webAdeApplications = null;
+    this.application = null;
   }
 
   @Override
   public Map<String, Object> getUserAttributes(
     final String businessApplicationName,
     final String consumerKey) {
-    final Map<String, Object> userAttributes = new LinkedHashMap<String, Object>();
+    final Map<String, Object> userAttributes = super.getUserAttributes(
+      businessApplicationName, consumerKey);
     final WebADEUserInfo userInfo = getUserInfo(businessApplicationName,
       consumerKey);
     if (userInfo != null) {
@@ -107,107 +56,22 @@ public class WebAdeAuthorizationService extends BaseAuthorizationService {
     return userAttributes;
   }
 
-  public UserCredentials getUserCredentials(
-    final String businessApplicationName,
-    final String consumerKey) {
-    WebADEUserInfo userInfo = getUserInfo(businessApplicationName,consumerKey);
-    return userInfo.getUserCredentials();
-  }
-
-  private UserCredentials getUserCredentials(final String consumerKey) {
-    final UserCredentials userCredentials = new UserCredentials();
-    userCredentials.setUserGuid(new GUID(consumerKey));
-    return userCredentials;
-  }
-
-  public WebADEUserInfo getUserInfo(
+  private WebADEUserInfo getUserInfo(
     final String businessApplicationName,
     final String consumerKey) {
     try {
-      final Application webAdeApplication = getWebAdeApplication(businessApplicationName);
-      if (webAdeApplication == null) {
+      if (application == null) {
         return null;
       } else {
-        final UserInfoService userInfoService = webAdeApplication.getUserInfoService();
-        final UserCredentials userCredentials = getUserCredentials(consumerKey);
-           final WebADEUserInfo userInfo = userInfoService.getWebADEUserInfo(userCredentials);
+        final UserInfoService userInfoService = application.getUserInfoService();
+        final UserCredentials userCredentials = new UserCredentials();
+        userCredentials.setUserGuid(new GUID(consumerKey));
+        final WebADEUserInfo userInfo = userInfoService.getWebADEUserInfo(userCredentials);
         return userInfo;
       }
     } catch (final UserInfoServiceException e) {
       throw new RuntimeException("Unable to get WebADE user info for "
         + consumerKey, e);
-    }
-  }
-
-  public WebADEUserPermissions getUserPermissions(
-    final String businessApplicationName,
-    final String consumerKey) {
-    try {
-      final Application webAdeApplication = getWebAdeApplication(businessApplicationName);
-      if (webAdeApplication == null) {
-        return null;
-      } else {
-        final UserCredentials userCredentials = getUserCredentials(consumerKey);
-            final WebADEUserPermissions userPermissions = webAdeApplication.getWebADEUserPermissions(userCredentials);
-        return userPermissions;
-      }
-    } catch (final WebADEException e) {
-      throw new RuntimeException("Unable to get WebADE user permissions for "
-        + consumerKey, e);
-    }
-  }
-
-  /**
-   * Get the WebADE {@link Application} for the business application name. If it
-   * could not be created a log error will be created and the cache set to null
-   * to prevent repeated attempts to create it. The applications are cached when
-   * created. s
-   * 
-   * @param businessApplicationName
-   * @return
-   */
-  public Application getWebAdeApplication(String businessApplicationName) {
-    synchronized (webAdeApplications) {
-      Application webAdeApplication = null;
-      businessApplicationName = businessApplicationName.toUpperCase();
-      if (webAdeApplications.containsKey(businessApplicationName)) {
-        webAdeApplication = webAdeApplications.get(businessApplicationName);
-      } else {
-        try {
-          webAdeApplication = WebADEApplicationUtils.createApplication(businessApplicationName);
-        } catch (final WebADEException e) {
-          LOG.error("Unable to get WebADE application for "
-            + businessApplicationName, e);
-        }
-        webAdeApplications.put(businessApplicationName, webAdeApplication);
-      }
-      return webAdeApplication;
-    }
-  }
-
-  @Override
-  public boolean isInGroup(
-    final String businessApplicationName,
-    final String consumerKey,
-    final String groupName) {
-    final Application webAdeApplication = getWebAdeApplication(businessApplicationName);
-    if (webAdeApplication == null) {
-      return false;
-    } else {
-      final AppRoles roles = webAdeApplication.getRoles();
-      if (roles == null) {
-        return false;
-      } else {
-        final Role role = roles.getRole(groupName);
-        final WebADEUserPermissions userPermissions = getUserPermissions(
-          businessApplicationName, consumerKey);
-        if (userPermissions == null) {
-          return false;
-        } else {
-          final boolean permission = userPermissions.isUserInRole(role);
-          return permission;
-        }
-      }
     }
   }
 
