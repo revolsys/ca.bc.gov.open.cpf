e171a3a905104c4fb248535eae645ec6c994a572
pxaustin <pxaustin@ae29540e-f523-aa4d-84ac-4afd0e2cd6fe>
2012-10-12 19:34:27 +0000

    4.0.0-SNAPSHOT
    git-svn-id: https://apps.bcgov/svn/cpf/source/trunk@210 ae29540e-f523-aa4d-84ac-4afd0e2cd6fe

--- scripts/cpf-ddl.sql
+++ scripts/cpf-ddl.sql
@@ -1,6 +1,6 @@
 -- Z:\Documents\eclipse\ca.bc.gov.cpf\scripts\cpf-ddl.sql
 --
--- Generated for Oracle 10g on Thu Sep 27  09:23:57 2012 by Server Generator 10.1.2.9.28
+-- Generated for Oracle 10g on Fri Oct 12  10:42:54 2012 by Server Generator 10.1.2.9.28
 
 
 SPOOL cpf-ddl.lst
