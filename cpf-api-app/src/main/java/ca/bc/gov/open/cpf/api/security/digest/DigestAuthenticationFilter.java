597077dbefa189bf90b981dafeff3bcd35394025
Paul Austin <paul.austin@revolsys.com>
2015-03-04 18:14:23 -0800

    license

--- cpf-api-app/src/main/java/ca/bc/gov/open/cpf/api/security/digest/DigestAuthenticationFilter.java
+++ cpf-api-app/src/main/java/ca/bc/gov/open/cpf/api/security/digest/DigestAuthenticationFilter.java
@@ -1,10 +1,11 @@
-/* Copyright 2004, 2005, 2006 2009 Acegi Technology Pty Limited
+/*
+ * Copyright © 2008-2015, Province of British Columbia
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package ca.bc.gov.open.cpf.api.security.digest;
 
 import java.io.IOException;
