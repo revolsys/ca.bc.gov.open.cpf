335c5aff032c8f8973982b5800e6e7edb7693190
paul <paul@PAUSTIN-WIN10>
2019-02-06 14:12:35 -0800

    use directory

--- cpf-api-app/src/main/java/ca/bc/gov/open/cpf/api/scheduler/BatchJobService.java
+++ cpf-api-app/src/main/java/ca/bc/gov/open/cpf/api/scheduler/BatchJobService.java
@@ -1127,7 +1127,8 @@ public class BatchJobService implements ModuleEventListener {
     if (!batchJob.isCancelled()) {
       final String resultFormat = batchJob.getValue(BatchJob.RESULT_DATA_CONTENT_TYPE);
       final String fileExtension = IoFactory.fileExtensionByMediaType(resultFormat);
-      final File structuredResultFile = FileUtil.newTempFile("result", "." + fileExtension);
+      final File resultDirectory = FileUtil.newTempDirectory("job-"+batchJobId, "-result");
+      final File structuredResultFile = new File(resultDirectory,"result" + fileExtension);
 
       try {
         final PathResource resource = new PathResource(structuredResultFile);
@@ -1139,7 +1140,7 @@ public class BatchJobService implements ModuleEventListener {
       } catch (final Throwable e) {
         throw new RuntimeException("Unable to save results", e);
       } finally {
-        FileUtil.delete(structuredResultFile);
+        FileUtil.deleteDirectory(resultDirectory, true);
       }
     }
   }
